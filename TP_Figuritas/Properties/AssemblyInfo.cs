4a2405a67ba7260653e3bc12c3a192303aaae66a
GS <gise_sch@hotmail.com>
2018-10-03 22:29:16 -0300

    Updated project location, removed unused and temporary files

--- TP_Figuritas/Properties/AssemblyInfo.cs
+++ TP_Figuritas/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tp_Figuritas")]
+[assembly: AssemblyTitle("TP_Figuritas")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tp_Figuritas")]
+[assembly: AssemblyProduct("TP_Figuritas")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e1d7dfd2-70f0-4a6c-b7e5-2e364c4c3e7a")]
+[assembly: Guid("deb7c19f-329c-40f1-adb0-aa4708d75c24")]
 
 // Version information for an assembly consists of the following four values:
 //
