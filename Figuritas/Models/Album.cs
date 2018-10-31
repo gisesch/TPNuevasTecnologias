b223be117b200df4c38be3b2228a76ef04546b9e
David Favretto <david.favretto@southworks.com>
2018-10-31 20:46:25 -0300

    Add Entity Models

---
+++ Figuritas/Models/Album.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Figuritas.Models
+{
+    public class Album
+    {
+        #region Propiedades
+        private String id;
+        /// <summary>
+        /// Identificador del album
+        /// </summary>
+        public String Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        private String nombre;
+        /// <summary>
+        /// Nombre del Album
+        /// </summary>
+        public String Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                if (value == nombre)
+                    return;
+                nombre = value;
+            }
+        }
+        private ICollection<Figurita> figuritas;
+        /// <summary>
+        /// Coleccion de figuritas del Album
+        /// </summary>
+        public ICollection<Figurita> Figuritas
+        {
+            get
+            {
+                return figuritas;
+            }
+            set
+            {
+                if (value == figuritas)
+                    return;
+                figuritas = value;
+            }
+        }
+        #endregion
+
+        public Album(String id)
+        {
+            this.id = id;
+        }
+    }
+}
